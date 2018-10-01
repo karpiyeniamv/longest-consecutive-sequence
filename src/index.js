74ef546a0bc0cc6b770f30e9b571100c61dc022a
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-10-01 17:22:54 +0300

    result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if (array.length ==0 )
+    {
+        return 0;
+    }
+
+    nums = [];
+    for (var i=0; i<array.length;i++)
+    {
+        nums[i]=array[i];
+    }
+    nums = array.sort(function compareNumbers(a, b) {
+        return a - b;
+    })
+    //console.log (nums);
+
+
+    longestStreak = 1;
+    currentStreak = 1;
+
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i-1]) {
+            if (nums[i] == nums[i-1]+1) {
+                currentStreak += 1;
+            }
+            else {
+                longestStreak = Math.max(longestStreak, currentStreak);
+                currentStreak = 1;
+            }
+        }
+    }
+
+    return longestStreak;
 }
